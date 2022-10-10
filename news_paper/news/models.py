3d8d2a88f51ac495ae8dd2302d5d24b852899bae
vadim <ivadimn@mail.ru>
2022-10-10 21:21:39 +0300

    created models classes (fields)

--- news_paper/news/models.py
+++ news_paper/news/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+
+class Post(models.Model):
+    article = "ARTS"
+    news = "NEWS"
+    TYPES = [
+        (news, "Новость"),
+        (article, "Статья"),
+    ]
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    type_post = models.CharField(max_length=4, choices=TYPES)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    rating = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through="PostCategory")
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    rating = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
